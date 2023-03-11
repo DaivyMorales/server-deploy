2954e283a827fdacf4e618f92e624df963118774
Daivy Morales <daivymorales7705@gmail.com>
2023-03-11 11:53:43 -0500

    add port

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(port);
+app.listen(PORT);
 
 app.get("/", (req, res) => {
   res.send("Your deploy has been sucess! 💪🏼");
 });
-console.log(`This server is running on port ${port}`);
+console.log(`This server is running on port ${PORT}`);
