d518ebfd4be4b7347beba3df2ca5571aec3455f1
Daivy Morales <daivymorales7705@gmail.com>
2023-03-11 12:03:12 -0500

    creating new route

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.listen(PORT);
 app.get("/", (req, res) => {
   res.send("Your deploy has been sucess! 💪🏼");
 });
+
+app.get("/product", (req, res) => {
+  res.send("Obtaining Product!");
+});
+
 console.log(`This server is running on port ${PORT}`);
